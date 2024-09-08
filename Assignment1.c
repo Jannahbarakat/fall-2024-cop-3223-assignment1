fe5accd8572df89f2983421133ff5076e5a7e854
Jannahbarakat <jannahbarakatt@gmail.com>
2024-09-08 21:05:52 +0000

    first commit

---
+++ Assignment1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+#define pi 3.14159
+
+double AskForUserInput();
+double CalculateDistance();
+double CalculatePerimeter();
+double CalculateArea();
+double CalculateWidth();
+double CalculateHeight();
+
+int main(int argc, char **argv)
